c8a3e9b5f1d9897322862be25f134603f9635343
M1ngD0ng <alswjd6807@naver.com>
2024-07-31 17:23:08 +0900

    Feat : 회원가입

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,5 +1,3 @@
-// src/components/SignupForm.jsx
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signup } from '../api/auth';
@@ -9,16 +7,11 @@ import '../components/styles/LoginForm.css';
 function SignupForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [companyName, setCompanyName] = useState('');
   const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [businessNumber, setBusinessNumber] = useState('');
   const [message, setMessage] = useState('');
-  const navigate = useNavigate(); // useNavigate 훅 사용
-
-  const validatePhoneNumber = (phone) => {
-    const phonePattern = /^010\d{4}\d{4}$/;
-    return phonePattern.test(phone);
-  };
+  const navigate = useNavigate();
 
   const validatePassword = (password) => {
     const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{10,}$/;
@@ -28,26 +21,21 @@ function SignupForm() {
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    if (!validatePhoneNumber(phoneNumber)) {
-      setMessage('전화번호 형식이 맞지 않습니다.');
-      return;
-    }
-
     if (!validatePassword(password)) {
       setMessage('비밀번호는 최소 10자 이상이어야 하며, 영문 대소문자, 숫자, 특수문자를 최소 1글자씩 포함해야 합니다.');
       return;
     }
 
     try {
-      await signup({ username, password, name, email, phoneNumber });
-      setMessage('Signup successful!');
-      alert('회원가입이 완료되었습니다! 로그인 페이지로 이동합니다.'); // 회원가입 완료 알림
+      await signup({ username, password, companyName, email, businessNumber });
+      setMessage('회원가입이 완료되었습니다! 로그인 페이지로 이동합니다.');
+      alert('회원가입이 완료되었습니다! 로그인 페이지로 이동합니다.');
       navigate('/host/login'); // 회원가입 성공 후 로그인 페이지로 이동
     } catch (error) {
-      if (error.message) {
-        setMessage(error.message); // 서버에서 받은 에러 메시지를 설정
+      if (error.response && error.response.data.message) {
+        setMessage(error.response.data.message); // 서버에서 받은 에러 메시지를 설정
       } else {
-        setMessage('Signup failed. Please check your information and try again.');
+        setMessage('회원가입에 실패했습니다. 정보를 확인하고 다시 시도해주세요.');
       }
     }
   };
@@ -56,7 +44,7 @@ function SignupForm() {
     <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
       <div className="card shadow custom-card">
         <div className="card-body">
-          <h2 className="card-title text-center mb-4">Signup</h2>
+          <h2 className="card-title text-center mb-4">회원가입</h2>
           <form onSubmit={handleSignup}>
             <div className="mb-3">
               <label htmlFor="username" className="form-label">
@@ -85,15 +73,15 @@ function SignupForm() {
               />
             </div>
             <div className="mb-3">
-              <label htmlFor="name" className="form-label">
-                이름
+              <label htmlFor="companyName" className="form-label">
+                기업명
               </label>
               <input
                 type="text"
                 className="form-control"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                id="companyName"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
                 required
               />
             </div>
@@ -111,21 +99,21 @@ function SignupForm() {
               />
             </div>
             <div className="mb-3">
-              <label htmlFor="phoneNumber" className="form-label">
-                전화번호
+              <label htmlFor="businessNumber" className="form-label">
+                사업자 번호
               </label>
               <input
                 type="text"
                 className="form-control"
-                id="phoneNumber"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
+                id="businessNumber"
+                value={businessNumber}
+                onChange={(e) => setBusinessNumber(e.target.value)}
                 required
               />
             </div>
             <div className="d-grid">
               <button type="submit" className="btn custom-btn">
-                Signup
+                회원가입
               </button>
             </div>
           </form>
