b9b1657c87dda94e29c011ea7fe9131c22cdb705
M1ngD0ng <alswjd6807@naver.com>
2024-08-02 16:36:39 +0900

    Refactor : 사용하지 않는 코드 제거

--- src/components/PopupDetail.jsx
+++ src/components/PopupDetail.jsx
@@ -1,13 +1,10 @@
 // src/components/PopupDetail.jsx
 
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react'; 
 import PopupInfo from './PopupInfo';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function PopupDetail() {
-  const { id } = useParams();
-
   return (
     <div>
       <PopupInfo />
