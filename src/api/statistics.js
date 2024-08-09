f3d24897698f6958e755dbead99da1b4ff24ef63
M1ngD0ng <alswjd6807@naver.com>
2024-08-10 00:02:43 +0900

    Refactor : Url 오타 수정

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -1,14 +1,14 @@
 import axiosInstance from './axiosInstance';
 
 export const getMonthlySalesStatistics = async (itemId) => {
-    const response = await axiosInstance.get(`/api/v1//sales-statistics/monthly`, {
+    const response = await axiosInstance.get(`/api/v1/sales-statistics/monthly`, {
       params: { itemId }
     });
     return response.data.data;
   };
 
 export const getDailySalesStatistics = async (popupId) => {
-    const response = await axiosInstance.get(`/api/v1//sales-statistics/recent-month`, {
+    const response = await axiosInstance.get(`/api/v1/sales-statistics/recent-month`, {
       params: { popupId }
     });
     return response.data.data;
