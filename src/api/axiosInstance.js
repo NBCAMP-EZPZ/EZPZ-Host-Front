3ca6099e2bb93e734a9af8da6d5690ae22755eb1
M1ngD0ng <alswjd6807@naver.com>
2024-08-13 21:09:32 +0900

    Refactor : package_json 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -20,5 +20,22 @@ axiosInstance.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
+// axios 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+      if (error.response && error.response.status === 401) {
+          // 여기서 리프레시 토큰 만료 시 로직을 처리합니다.
+          alert('로그인이 만료되었습니다. 다시 로그인해주세요.');
+          localStorage.removeItem('accessToken'); // 만료된 토큰 제거
+          window.location.href = '/host/login'; // 로그인 페이지로 리디렉션
+      } else if (error.response && error.response.status === 403) {
+          alert('권한이 없습니다. 로그인을 하고 시도해주세요.');
+          window.location.href = '/host/login'; // 로그인 페이지로 리디렉션
+      }
+
+      return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
